2262bfe872c176f27100fe43ac5133b06d7ca3a0
Aitor González Fernández <info@aitorgf.com>
2020-04-05 01:26:23 +0200

    FIX: Add support for /sno API (Storj >= 1.0.0)

--- monitors/storj.go
+++ monitors/storj.go
@@ -8,21 +8,21 @@ import (
 	"github.com/tokkenno/storj-prometheus-exporter/sensors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
 type StorjMonitor struct {
-	url string
+	url     string
 	Refresh time.Duration
 }
 
 func NewStorjMonitor(url string) *StorjMonitor {
 	return &StorjMonitor{
-		url: url,
+		url:     url,
 		Refresh: 5 * time.Second,
 	}
 }
-
 
 func (mon *StorjMonitor) Start() {
 	go func() {
@@ -34,10 +34,76 @@ func (mon *StorjMonitor) Start() {
 }
 
 func (mon *StorjMonitor) update() {
-	err := mon.UpdateDashboard(mon.url)
+	err := mon.UpdateSNO(mon.url)
 	if err != nil {
+		log.Warn(fmt.Sprintf("Error while update node <%s>:", mon.url))
 		log.Warn(err)
 	}
+}
+
+func (mon *StorjMonitor) UpdateSNO(url string) error {
+	start := time.Now()
+	resp, err := http.Get(fmt.Sprintf("http://%s/api/sno/", url))
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if strings.Contains(string(body), "<!DOCTYPE html>") {
+		// Fallback to pre-1.0.0 API
+		return mon.UpdateDashboard(url)
+	}
+
+	requestInfo := new(models.RequestInfo)
+	requestInfo.Duration = time.Since(start)
+
+	var dashboard models.DashboardData
+	err = json.Unmarshal(body, &dashboard)
+	if err != nil {
+		return err
+	}
+
+	sensors.SetDashboardInfo(url, &dashboard)
+	sensors.SetRequestInfo(url, dashboard.NodeID, requestInfo)
+
+	for _, satellite := range dashboard.Satellites {
+		err := mon.UpdateSNOSatellite(url, dashboard.NodeID, satellite.ID)
+		if err != nil {
+			log.Warnf("The satellite %s can't be updated for node %s", satellite.ID, url)
+		}
+	}
+
+	return nil
+}
+
+func (mon *StorjMonitor) UpdateSNOSatellite(url string, nodeId string, satelliteId string) error {
+	start := time.Now()
+	resp, err := http.Get(fmt.Sprintf("http://%s/api/sno/satellite/%s", url, satelliteId))
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	requestInfo := new(models.RequestInfo)
+	requestInfo.Duration = time.Since(start)
+
+	satellite := new(models.SatelliteData)
+	err = json.Unmarshal(body, satellite)
+	if err != nil {
+		return err
+	}
+
+	sensors.SetSatelliteInfo(url, nodeId, satelliteId, *satellite)
+
+	return nil
 }
 
 func (mon *StorjMonitor) UpdateDashboard(url string) error {
